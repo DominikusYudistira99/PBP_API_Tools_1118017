56d3211744120bbebebdfbff6a96086c8f52d280
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2023-03-29 13:16:19 +0700

    GoCRON main.go

---
+++ GoCRON/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+type Student struct {
+	ID    int
+	Name  string
+	Grade int
+}
+
+func main() {
+	students := []Student{
+		{ID: 1, Name: "John Doe", Grade: 85},
+		{ID: 2, Name: "Jane Smith", Grade: 92},
+		{ID: 3, Name: "Bob Johnson", Grade: 70},
+		{ID: 4, Name: "Sarah Lee", Grade: 88},
+		{ID: 5, Name: "Tom Wilson", Grade: 78},
+	}
+
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(10).Seconds().Do(func() {
+		var wg sync.WaitGroup
+		wg.Add(len(students))
+
+		for _, student := range students {
+			go func(s Student) {
+				processStudent(&s)
+				wg.Done()
+			}(student)
+		}
+
+		wg.Wait()
+		fmt.Println("All students processed!")
+	})
+
+	s.StartBlocking()
+}
+
+func processStudent(student *Student) {
+	// Perform some processing on student data
+	if student.Grade >= 85 {
+		fmt.Printf("%s (%d): Anda mendapatkan A\n", student.Name, student.ID)
+	} else if student.Grade >= 70 {
+		fmt.Printf("%s (%d): Anda Mendapatkan B\n", student.Name, student.ID)
+	} else {
+		fmt.Printf("%s (%d): Anda Mendapatkan C \n", student.Name, student.ID)
+	}
+}
